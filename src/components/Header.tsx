6ca333c0ace821fedbf81f0e5468b7b6d2b41ba9
MilosNj <njegovanovicm@yahoo.com>
2022-05-23 18:47:12 +0200

    Add header spacing

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,8 +10,14 @@ const Header = () => {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link href="/cart">Cart</Nav.Link>
-              <Nav.Link href="/login">Sign In</Nav.Link>
+              <Nav.Link href="/cart">
+                <i className="fas fa-shopping-cart pe-2" />
+                Cart
+              </Nav.Link>
+              <Nav.Link href="/login">
+                <i className="fas fa-user pe-2" />
+                Sign In
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
