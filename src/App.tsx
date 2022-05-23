660c890bc5daf03ffc7abdd70546a935a28333ec
MilosNj <njegovanovicm@yahoo.com>
2022-05-23 18:49:20 +0200

    Added padding for main

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ const App = () => {
   return (
     <>
       <Header />
-      <main>
+      <main className="py-3">
         <Container>
           <h1>Welcome to Online Jewelry</h1>
         </Container>
