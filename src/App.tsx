ffb719ea0d0655da7035dffaa4811a714c2137aa
MilosNj <njegovanovicm@yahoo.com>
2022-05-23 17:57:55 +0200

    Set up header and footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 const App = () => {
   return (
     <>
+      <Header />
       <h1>Welcome to Online Jewelry</h1>
+      <Footer />
     </>
   );
 };
