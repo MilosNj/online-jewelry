12f0b9f44587600dd80d2eacef0bd439419ff78c
MilosNj <njegovanovicm@yahoo.com>
2022-05-24 19:34:38 +0200

    Set up NotFound route and fix height of layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,26 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
+import NotFound from "./screens/NotFound";
 
 const App = () => {
   return (
-    <>
+    <BrowserRouter>
       <Header />
       <main className="py-3">
         <Container>
-          <HomeScreen />
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
         </Container>
       </main>
       <Footer />
-    </>
+    </BrowserRouter>
   );
 };
 
