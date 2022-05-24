5058ddd7e8322b7f04829cd66f0c335eeaccfeb7
MilosNj <njegovanovicm@yahoo.com>
2022-05-24 19:59:08 +0200

    Implemented navbar routing

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
 import NotFound from "./screens/NotFound";
+import ProductScreen from "./screens/ProductScreen";
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
         <Container>
           <Routes>
             <Route path="/" element={<HomeScreen />} />
+            <Route path="/product/:id" element={<ProductScreen />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </Container>
