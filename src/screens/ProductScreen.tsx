ebe0dde4ffc1b7b7dba76ae04ad1fa8478f2f387
MilosNj <njegovanovicm@yahoo.com>
2022-05-25 11:29:51 +0200

    Show product name on product screen

--- src/screens/ProductScreen.tsx
+++ src/screens/ProductScreen.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+
+import products from "../products";
 
 const ProductScreen = () => {
-  return <div>Product</div>;
+  const params = useParams();
+  const product = products.find((product) => product._id === params.id);
+
+  return <div>{product?.name}</div>;
 };
 
 export default ProductScreen;
