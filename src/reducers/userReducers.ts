081298126a53d8f342980c598d8b00e5b566f778
milos-njegovanovic-ey <milos.njegovanovic@rs.ey.com>
2022-06-27 15:28:09 +0200

    Added user loigin reducer

---
+++ src/reducers/userReducers.ts
@@ -0,0 +1,23 @@
+import { PayloadAction } from "@reduxjs/toolkit";
+
+import {
+  USER_LOGIN_FAIL,
+  USER_LOGIN_REQUEST,
+  USER_LOGIN_SUCCESS,
+  USER_LOGOUT,
+} from "../constants/userConstants";
+
+export const userLoginReducer = (state = {}, action: PayloadAction) => {
+  switch (action.type) {
+    case USER_LOGIN_REQUEST:
+      return { loading: true };
+    case USER_LOGIN_SUCCESS:
+      return { loading: false, userInfo: action.payload };
+    case USER_LOGIN_FAIL:
+      return { loading: false, error: action.payload };
+    case USER_LOGOUT:
+      return {};
+    default:
+      return state;
+  }
+};
--- src/store.ts
+++ src/store.ts
@@ -5,11 +5,13 @@ import {
   productDetailsReducer,
   productListReducer,
 } from "./reducers/productReducers";
+import { userLoginReducer } from "./reducers/userReducers";
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
+  userLogin: userLoginReducer,
 });
 
 const store = configureStore({
