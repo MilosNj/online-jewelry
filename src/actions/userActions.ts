a554663ee2bb42724c44b923050f5274fc5a4c37
milos-njegovanovic-ey <milos.njegovanovic@rs.ey.com>
2022-06-27 16:04:37 +0200

    Addde user actions

---
+++ src/actions/userActions.ts
@@ -0,0 +1,40 @@
+import axios, { AxiosError } from "axios";
+import { Dispatch } from "react";
+
+import {
+  USER_LOGIN_FAIL,
+  USER_LOGIN_REQUEST,
+  USER_LOGIN_SUCCESS,
+} from "../constants/userConstants";
+
+export const login =
+  (email: string, password: string) => async (dispatch: Dispatch<any>) => {
+    try {
+      dispatch({
+        type: USER_LOGIN_REQUEST,
+      });
+
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+
+      const { data } = await axios.post(
+        "/users/login",
+        { email, password },
+        config
+      );
+
+      dispatch({
+        type: USER_LOGIN_SUCCESS,
+        payload: data,
+      });
+
+      localStorage.setItem("userInfo", JSON.stringify(data));
+    } catch (error) {
+      const err = error as AxiosError;
+
+      dispatch({ type: USER_LOGIN_FAIL, payload: err.message });
+    }
+  };
