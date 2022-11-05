a4498538c2c17af0101e24988d0d5bff34672712
milos-njegovanovic-ey <milos.njegovanovic@rs.ey.com>
2022-11-05 14:13:20 +0100

    renormalized the CRLF to LF and set up different Prettier properties

--- src/actions/userActions.ts
+++ src/actions/userActions.ts
@@ -1,46 +1,46 @@
-import axios, { AxiosError } from "axios";
-import { Dispatch } from "react";
+import axios, { AxiosError } from 'axios'
+import { Dispatch } from 'react'
 
 import {
-  USER_LOGIN_FAIL,
-  USER_LOGIN_REQUEST,
-  USER_LOGIN_SUCCESS,
-  USER_LOGOUT,
-} from "../constants/userConstants";
+    USER_LOGIN_FAIL,
+    USER_LOGIN_REQUEST,
+    USER_LOGIN_SUCCESS,
+    USER_LOGOUT
+} from '../constants/userConstants'
 
 export const login =
-  (email: string, password: string) => async (dispatch: Dispatch<any>) => {
-    try {
-      dispatch({
-        type: USER_LOGIN_REQUEST,
-      });
+    (email: string, password: string) => async (dispatch: Dispatch<any>) => {
+        try {
+            dispatch({
+                type: USER_LOGIN_REQUEST
+            })
 
-      const config = {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      };
+            const config = {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
 
-      const { data } = await axios.post(
-        "/users/signin",
-        { email, password },
-        config
-      );
+            const { data } = await axios.post(
+                '/users/signin',
+                { email, password },
+                config
+            )
 
-      dispatch({
-        type: USER_LOGIN_SUCCESS,
-        payload: data,
-      });
+            dispatch({
+                type: USER_LOGIN_SUCCESS,
+                payload: data
+            })
 
-      localStorage.setItem("userInfo", JSON.stringify(data));
-    } catch (error) {
-      const err = error as AxiosError;
+            localStorage.setItem('userInfo', JSON.stringify(data))
+        } catch (error) {
+            const err = error as AxiosError
 
-      dispatch({ type: USER_LOGIN_FAIL, payload: err.message });
+            dispatch({ type: USER_LOGIN_FAIL, payload: err.message })
+        }
     }
-  };
 
 export const logout = () => async (dispatch: Dispatch<any>) => {
-  localStorage.removeItem("userInfo");
-  dispatch({ type: USER_LOGOUT });
-};
+    localStorage.removeItem('userInfo')
+    dispatch({ type: USER_LOGOUT })
+}
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,58 +1,63 @@
-import { LinkContainer } from "react-router-bootstrap";
-import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
+import { LinkContainer } from 'react-router-bootstrap'
+import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap'
 
-import { useAppDispatch, useAppSelector } from "../hooks";
-import { logout } from "../actions/userActions";
+import { useAppDispatch, useAppSelector } from '../hooks'
+import { logout } from '../actions/userActions'
 
 const Header = () => {
-  const dispatch = useAppDispatch();
+    const dispatch = useAppDispatch()
 
-  const userLogin: any = useAppSelector((state) => state.userLogin);
-  const { userInfo } = userLogin;
+    const userLogin: any = useAppSelector((state) => state.userLogin)
+    const { userInfo } = userLogin
 
-  const logoutHandler = () => {
-    dispatch(logout());
-  };
+    const logoutHandler = () => {
+        dispatch(logout())
+    }
 
-  return (
-    <header>
-      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
-        <Container>
-          <LinkContainer to="/">
-            <Navbar.Brand>Online Jewelry</Navbar.Brand>
-          </LinkContainer>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto">
-              <LinkContainer to="/cart">
-                <Nav.Link>
-                  <i className="fas fa-shopping-cart pe-2" />
-                  Cart
-                </Nav.Link>
-              </LinkContainer>
-              {userInfo ? (
-                <NavDropdown title={userInfo.name} id="username">
-                  <LinkContainer to="/profile">
-                    <NavDropdown.Item>Profile</NavDropdown.Item>
-                  </LinkContainer>
-                  <NavDropdown.Item onClick={logoutHandler}>
-                    Sign Out
-                  </NavDropdown.Item>
-                </NavDropdown>
-              ) : (
-                <LinkContainer to="/login">
-                  <Nav.Link>
-                    <i className="fas fa-user pe-2" />
-                    Sign In
-                  </Nav.Link>
-                </LinkContainer>
-              )}
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </header>
-  );
-};
+    return (
+        <header>
+            <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+                <Container>
+                    <LinkContainer to='/'>
+                        <Navbar.Brand>Online Jewelry</Navbar.Brand>
+                    </LinkContainer>
+                    <Navbar.Toggle aria-controls='basic-navbar-nav' />
+                    <Navbar.Collapse id='basic-navbar-nav'>
+                        <Nav className='ms-auto'>
+                            <LinkContainer to='/cart'>
+                                <Nav.Link>
+                                    <i className='fas fa-shopping-cart pe-2' />
+                                    Cart
+                                </Nav.Link>
+                            </LinkContainer>
+                            {userInfo ? (
+                                <NavDropdown
+                                    title={userInfo.name}
+                                    id='username'
+                                >
+                                    <LinkContainer to='/profile'>
+                                        <NavDropdown.Item>
+                                            Profile
+                                        </NavDropdown.Item>
+                                    </LinkContainer>
+                                    <NavDropdown.Item onClick={logoutHandler}>
+                                        Sign Out
+                                    </NavDropdown.Item>
+                                </NavDropdown>
+                            ) : (
+                                <LinkContainer to='/login'>
+                                    <Nav.Link>
+                                        <i className='fas fa-user pe-2' />
+                                        Sign In
+                                    </Nav.Link>
+                                </LinkContainer>
+                            )}
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </header>
+    )
+}
 
-export default Header;
+export default Header
